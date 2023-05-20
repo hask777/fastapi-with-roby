4d1fbe18a04e8ae887704ecc020370c2b2b70a2d
hask777 <haskellisp@gmail.com>
2023-05-20 21:44:00 +0300

    save users to postgresql

--- todoApp/database.py
+++ todoApp/database.py
@@ -2,10 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todos.db'
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:lara@localhost/TodoApplicationDatabase'
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
